af0e45ecda2af4fd1fa638502887807b4a9cb977
RK JONY <rezaulkarim.imraan@gmail.com>
2025-04-27 18:15:53 +0600

    sfda

--- Package.swift
+++ Package.swift
@@ -1,29 +1,26 @@
-// swift-tools-version: 6.1
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
     name: "TNLogger",
-    platforms: [.iOS(.v16)],
+    platforms: [
+        .iOS(.v16)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "TNLogger",
-            targets: ["TNLogger"]),
+            targets: ["TNLogger"] // <-- expects a target named "TNLogger"
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-                    name: "YourPackage",
-                    dependencies: [],
-                    swiftSettings: [
-                        .define("DEV"), // <-- Add your custom build flags here
-                        .define("QA"), // <-- Add your custom build flags here
-                        .define("PROD") // <-- Add your custom build flags here
-                    ]
-                ),
-
+            name: "TNLogger", // <-- now matches!
+            dependencies: [],
+            swiftSettings: [
+                .define("DEV"),
+                .define("QA"),
+                .define("PROD")
+            ]
+        ),
     ]
 )
